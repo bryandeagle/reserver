b5393ddabeee5d5bfdea1127cc250dbac89e28db
= <bryan@dea.gl>
2021-10-06 08:48:06 -0500

    added success file capabilities

--- res.py
+++ res.py
@@ -9,100 +9,95 @@ _LOGGER = logging.getLogger(__name__)
 _LOGGER.addHandler(logging.NullHandler())
 
 
-class Resy:
-    def __init__(self, username, password, venue, party, date, times):
-        _LOGGER.info('Initializing application')
-        self.headers = {
-            'origin': 'https://resy.com',
-            'accept-encoding': 'gzip, deflate, br',
-            'x-origin': 'https://resy.com',
-            'accept-language': 'en-US,en;q=0.9',
-            'authorization': 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
-            'content-type': 'application/x-www-form-urlencoded',
-            'accept': 'application/json, text/plain, */*',
-            'referer': 'https://resy.com/',
-            'authority': 'api.resy.com',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
-        }
-        self.username = username
-        self.password = password
-        self.venue = venue
-        self.party = party
-        self.date = date
-        self.times = times
-        self.datetimes = ['{} {}'.format(self.date, t) for t in self.times]
-        self.token, self.payment_method = self._login(username, password)
+def resy(username, password, venue, party, date, times, dont_reserve):
+    _LOGGER.info('Initializing application')
+    headers = {
+        'origin': 'https://resy.com',
+        'accept-encoding': 'gzip, deflate, br',
+        'x-origin': 'https://resy.com',
+        'accept-language': 'en-US,en;q=0.9',
+        'authorization': 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
+        'content-type': 'application/x-www-form-urlencoded',
+        'accept': 'application/json, text/plain, */*',
+        'referer': 'https://resy.com/',
+        'authority': 'api.resy.com',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+    }
+    username = username
+    password = password
+    venue = venue
+    party = party
+    date = date
+    times = times
+    datetimes = ['{} {}'.format(date, t) for t in times]
 
-    def _login(self, username,password):
-        """ Log into Resy """
-        data = {'email': username, 'password': password}
-        response = requests.post('https://api.resy.com/3/auth/password', headers=self.headers, data=data)
-        
-        # Parse response
-        details = response.json()
-        if 'token' not in details:
-            _LOGGER.error(response.text)
-            raise ValueError(response.text)
-        
-        payment_method = '{\"id\":' + str(details['payment_method_id']) + '}'
-        _LOGGER.info('Successfully logged in')
-        return details['token'], payment_method
+    ################################
+    # Log Into Resy
+    ################################
 
-    def search(self):
-        """Search for reservations matching desired times"""
-        # Format and send request
-        params = (
-            ('x-resy-auth-token',  self.token),
-            ('day', self.date),
-            ('lat', '0'),
-            ('long', '0'),
-            ('party_size', self.party),
-            ('venue_id',self.venue),
-        )
-        response = requests.get('https://api.resy.com/4/find',
-                                headers=self.headers,
-                                params=params)
-        # Parse response
-        details = response.json()
-        if 'results' not in details:
-            _LOGGER.error(response.text)
-            raise ValueError(response.text)
-        
-        # Log venue name
-        venue_name = details['results']['venues'][0]['venue']['name']
-        _LOGGER.info('Venue: {} ({})'.format(venue_name, self.venue))
+    data = {'email': username, 'password': password}
+    response = requests.post('https://api.resy.com/3/auth/password', headers=headers, data=data)
+    
+    # Parse response
+    details = response.json()
+    if 'token' not in details:
+        _LOGGER.error(response.text)
+        raise ValueError(response.text)
+    
+    payment_method = '{\"id\":' + str(details['payment_method_id']) + '}'
+    _LOGGER.info('Successfully logged in')
+    token = details['token']
+    payment_method = payment_method
+    
+    ################################
+    # Search for Tables
+    ################################
 
-        # Loop through returned slots to find the best one
-        best_slot, best_score = None, len(self.datetimes) + 1
-        for slot in details['results']['venues'][0]['slots']:
-            slot_time = slot['date']['start']
-            if slot_time in self.datetimes:
-                score = self.datetimes.index(slot_time)
-                if score < best_score:
-                    best_slot = slot
-                    best_score = score
-        if best_slot:
-            _LOGGER.info('Best slot: {}'.format(best_slot['date']['start']))
-        else:
-            _LOGGER.info('No valid slots found.')
-        return best_slot
+    # Format and send request
+    params = (
+        ('x-resy-auth-token',  token),
+        ('day', date),
+        ('lat', '0'),
+        ('long', '0'),
+        ('party_size', party),
+        ('venue_id',venue),
+    )
+    response = requests.get('https://api.resy.com/4/find',
+                            headers=headers,
+                            params=params)
+    # Parse response
+    details = response.json()
+    if 'results' not in details:
+        _LOGGER.error(response.text)
+        raise ValueError(response.text)
+    
+    # Log venue name
+    venue_name = details['results']['venues'][0]['venue']['name']
+    _LOGGER.info('Venue: {} ({})'.format(venue_name, venue))
 
-    def reserve(self):
-        """Book a reservation"""
-        # Find table
-        slot = self.search()
-        if slot is None:
-            return
+    # Loop through returned slots to find the best one
+    best_slot, best_score = None, len(datetimes) + 1
+    for slot in details['results']['venues'][0]['slots']:
+        slot_time = slot['date']['start']
+        if slot_time in datetimes:
+            score = datetimes.index(slot_time)
+            if score < best_score:
+                best_slot = slot
+                best_score = score
+    if best_slot and dont_reserve == 'False':
+        _LOGGER.info('Best slot: {}'.format(best_slot['date']['start']))
+        ################################
+        # Make Reservation
+        ################################
         
-        # Format and send request
-        params = (
-            ('x-resy-auth-token', self.token),
-            ('config_id', slot['config']['token']),
-            ('day', self.date),
-            ('party_size', self.party),
+        params = (  # Format and send request
+            ('x-resy-auth-token', token),
+            ('config_id', best_slot['config']['token']),
+            ('day', date),
+            ('party_size', party),
         )
         response = requests.get('https://api.resy.com/3/details',
-                                headers=self.headers,
+                                headers=headers,
                                 params=params)
         # Parse response
         details = response.json()
@@ -111,28 +106,45 @@ class Resy:
             raise ValueError(response.text)
         
         # Make reservation request
-        self.headers['x-resy-auth-token'] = self.token
+        headers['x-resy-auth-token'] = token
         data = {
             'book_token': details['book_token']['value'],
-            'struct_payment_method': self.payment_method,
+            'struct_payment_method': payment_method,
             'source_id': 'resy.com-venue-details'
         }
         _LOGGER.info('Making Reservation')
         response = requests.post('https://api.resy.com/3/book',
-                             headers=self.headers,
+                             headers=headers,
                              data=data)
-        # Parse response
-        details = response.json()
+
+        details = response.json()  # Parse response
         if 'reservation_id' not in details:
             _LOGGER.error(response.text)
             raise ValueError(response.text)
         _LOGGER.info('Reservation ID: {}'.format(details['reservation_id']))
+        return True
+    else:
+        _LOGGER.info('No valid slots found.')
+    return False
 
 
-def readconfig():
+def read_config():
     config_file = os.path.join(os.path.dirname(__file__), 'config.json')
     with open(config_file, 'rt') as f:
         return json.load(f)
+
+
+def read_success():
+    """Returns true if success file exists"""
+    success_file = os.path.join(os.path.dirname(__file__), 'success')
+    return os.path.exists(success_file)
+
+
+def write_success():
+    """Creates success file"""
+    success_file = os.path.join(os.path.dirname(__file__), 'success')
+    with open(success_file, 'wt') as f:
+        f.write('success')
 
 
 def start_logging(verbose=False):
@@ -154,7 +166,13 @@ def start_logging(verbose=False):
 
 
 if __name__ == '__main__':
-    start_logging(verbose=False)
-    resy = Resy(**readconfig())
-    resy.search()
-    #resy.reserve()
+    start_logging(verbose=True)
+    if read_success():
+        _LOGGER.info('Success file found - Skipping')
+    else:
+        _LOGGER.info('No success file found - Proceeding')
+        success = resy(**read_config())
+        if success:
+            write_success()
+            _LOGGER.info('Created success file')
+    _LOGGER.info('Exiting')
